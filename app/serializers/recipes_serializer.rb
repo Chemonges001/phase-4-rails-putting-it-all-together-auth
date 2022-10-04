c3059e7720665111719570a91905654a8bb027ac
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-10-04 23:05:25 +0300

    commit tree c4b7468e2a389c9b6263d0fb019df1a2de0b3a3a

---
+++ app/serializers/recipes_serializer.rb
@@ -0,0 +1,4 @@
+class RecipesSerializer < ActiveModel::Serializer
+  attributes :id, :title, :instructions, :minutes_to_complete, :user_id
+  has_one :user 
+end
---
+++ db/migrate/20221004192711_instructions_change_column_type.rb
@@ -0,0 +1,5 @@
+class InstructionsChangeColumnType < ActiveRecord::Migration[6.1]
+  def change
+    change_column(:recipes, :instructions, :text)
+  end
+end
